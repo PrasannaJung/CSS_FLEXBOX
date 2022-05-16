d0249af8d2897c553931f81fc0d8aece5cabee7f
PrasannaJung <prasannajung08@gmail.com>
2022-05-16 23:14:44 +0545

    correct functionalities add

--- src/components/Flex.js
+++ src/components/Flex.js
@@ -1,105 +1,89 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import './Flex.css';
 
 const Flex = () => {
   const containerRef = useRef();
 
-  const makeFlexHandler = () => {
-    containerRef.current.classList.toggle('flex');
-    console.log(containerRef.current);
-  };
-  const flexRowHandler = () => {
-    containerRef.current.classList.toggle('flex-row');
-  };
-  const flexColumnHandler = () => {
-    containerRef.current.classList.toggle('flex-column');
-  };
-  const justifyCenterHandler = () => {
-    containerRef.current.classList.toggle('justify-content-center');
-  };
-  const justifyAroundHandler = () => {
-    containerRef.current.classList.toggle('justify-content-around');
-  };
-  const justifyBetweenHandler = () => {
-    containerRef.current.classList.toggle('justify-content-between');
-  };
-  const justifyEvenlyHandler = () => {
-    containerRef.current.classList.toggle('justify-content-evenly');
-  };
-
-  const justifyStartHandler = () => {
-    containerRef.current.classList.toggle('justify-content-start');
-  };
-  const justifyEndHandler = () => {
-    containerRef.current.classList.toggle('justify-content-end');
-  };
-  const alignCenterHandler = () => {
-    containerRef.current.classList.toggle('align-items-center');
-  };
-
-  const alignStretchHandler = () => {
-    containerRef.current.classList.toggle('align-items-stretch');
-  };
-  const alignBaselineHandler = () => {
-    containerRef.current.classList.toggle('align-items-baseline');
-  };
-  const alignStartHandler = () => {
-    containerRef.current.classList.toggle('align-items-flex-start');
-  };
-  const alignEndHandler = () => {
-    containerRef.current.classList.toggle('align-items-flex-end');
-  };
+  const [flexDirValue, setFlexDirValue] = useState('flex-row');
+  const [flexJustifyValue, setFlexJustifyValue] = useState('flex-start');
+  const [flexAlignValue, setFlexAlignValue] = useState('flex-start');
 
   return (
     <>
-      <section ref={containerRef} className='flex-container'>
+      <section
+        ref={containerRef}
+        className={`flex-container ${flexDirValue} ${flexJustifyValue} ${flexAlignValue}`}
+      >
         <div className='box'></div>
         <div className='box'></div>
         <div className='box'></div>
       </section>
       <section className='buttons'>
-        <button className='btn' onClick={makeFlexHandler}>
-          Flex
-        </button>
-        <button className='btn' onClick={flexRowHandler}>
-          Flex-Row
-        </button>
-        <button onClick={flexColumnHandler} className='btn'>
-          Flex-Column
-        </button>
-        <button onClick={justifyCenterHandler} className='btn'>
-          Justify-Center
-        </button>
-        {/* <button onClick={justifyAroundHandler} className='btn'>
-          Justify-Around
-        </button>
-        <button onClick={justifyBetweenHandler} className='btn'>
-          Justify-Between
-        </button>
-        <button onClick={justifyEvenlyHandler} className='btn'>
-          Justify-Evenly
-        </button>
-        <button onClick={justifyStartHandler} className='btn'>
-          Justify-Start
-        </button>
-        <button onClick={justifyEndHandler} className='btn'>
-          Justify-End
-        </button> */}
-        <button onClick={alignCenterHandler} className='btn'>
-          Align-Center
-        </button>
-        {/* <button onClick={alignStretchHandler} className='btn'>
-          Align-Stretch
-        </button>
-        <button onClick={alignBaselineHandler} className='btn'>
-          Align-Baseline
-        </button>
-        <button onClick={alignStartHandler} className='btn'>
-          Align-Start
-        </button>
-        <button onClick={alignEndHandler} className='btn'>
-          Align-End
-        </button> */}
+        <select
+          onChange={e => {
+            setFlexDirValue(e.target.value);
+          }}
+        >
+          <option className='' value='' disabled>
+            --Select Flex Direction--
+          </option>
+          <option className='' value='flex-row'>
+            Row
+          </option>
+          <option className='' value='flex-column'>
+            Column
+          </option>
+        </select>
+        <select
+          onChange={e => {
+            setFlexJustifyValue(e.target.value);
+          }}
+        >
+          <option className='' value='' disabled>
+            --Select Justify Value--
+          </option>
+          <option className='' value='justify-start'>
+            Justify-Start
+          </option>
+          <option className='' value='justify-center'>
+            Justify-Center
+          </option>
+          <option className='' value='justify-around'>
+            Justify-Around
+          </option>
+          <option className='' value='justify-evenly'>
+            Justify-Evenly
+          </option>
+          <option className='' value='justify-between'>
+            Justify-Between
+          </option>
+          <option className='' value='justify-end'>
+            Justify-End
+          </option>
+        </select>
+
+        {/* Text Size */}
+        <select
+          onChange={e => {
+            setFlexAlignValue(e.target.value);
+          }}
+        >
+          <option className='' value='' disabled>
+            --Select Align Value--
+          </option>
+          <option className='' value='align-start'>
+            Align-Start
+          </option>
+          <option className='' value='align-center'>
+            Align-Center
+          </option>
+          <option className='' value='align-baseline'>
+            Align-Baseline
+          </option>
+          <option className='' value='align-end'>
+            Align End
+          </option>
+        </select>
       </section>
     </>
   );
